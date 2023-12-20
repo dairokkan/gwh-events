28a8fb4e4232ffc04e8faab1fbab77ffaf715921
dairokkan <shadow.27@skiff.com>
2023-12-20 07:55:02 +0530

    do we really need an eb page? part 2

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from './pages/Index.vue'
-import Team from './pages/Team.vue'
+//import Team from './pages/Team.vue'
 import Committees from "./pages/Committees.vue";
 import Resources from "./pages/Resources.vue";
 import CommitteePage from "./pages/CommitteePage.vue"
@@ -9,7 +9,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: Index},
-        {path: '/team', component: Team},
+        //{path: '/team', component: Team},
         {path: '/committees', component: Committees},
         {path: '/resources', component: Resources},
         {path: '/committees/unsc', component: CommitteePage, props: {
